fe4e8862fae7b0c76119a8405e92a754bf0c9859
Cássio Morais <cassiomoraisg@hotmail.com>
2021-02-15 12:43:23 -0300

    add: simulação aluguel de veículo com valor veículo mockado

---
+++ api/Controllers/SimulacaoLocacaoController.cs
@@ -0,0 +1,41 @@
+﻿using api.Domain.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    public class SimulacaoLocacaoController : BaseApiController
+    {
+        private IMapper _mapper;
+
+        public SimulacaoLocacaoController(
+            ILogger<SimulacaoLocacaoController> logger,
+            IMapper mapper
+
+        ) :
+            base(logger, mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public SimulacaoViewModel Simular([FromBody] SimulacaoViewModel simulacaoVM)
+        {
+            var tempodoAluguel = (simulacaoVM.Saida - simulacaoVM.Entrada).TotalHours;
+            // mock valor hora
+            simulacaoVM.veiculo.ValorHora = 10;
+            simulacaoVM.ValorSimulado = tempodoAluguel * simulacaoVM.veiculo.ValorHora;
+
+            return simulacaoVM;
+        }
+
+
+    }
+
+
+}
---
+++ api/Domain/ViewModels/SimulacaoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using api.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Domain.ViewModels
+{
+    public class SimulacaoViewModel
+    {
+        public Veiculo veiculo { get; set; }
+
+        public DateTime Entrada { get; set; }
+
+        public DateTimeOffset Saida { get; set; }
+
+        public double ValorSimulado { get; set; }
+    }
+}
